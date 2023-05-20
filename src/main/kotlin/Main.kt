5786e9c973541427a396e15bf39041c211c69a6e
Vasilii Karmanov <oldheffalump1@gmail.com>
2023-05-21 00:55:32 +0300

    solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,76 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println(translations(12000, 10000, "Visa"))
+    println(translations(590_000, 11000, "Мир"))
+    println(translations(29_000, 12000, "VkPay"))
+    println(translations(59_000, 18000, "Mastercard"))
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
 }
+
+fun maxMountLimitOtherCard(
+    amountOfPreviousTransfersMonth: Int,
+    transferAmount: Int,
+    maxMountLimitOtherCard: Int
+): Boolean {
+    return maxMountLimitOtherCard < (amountOfPreviousTransfersMonth + transferAmount)
+}
+
+fun maxMountLimitOtherCardText(amountOfPreviousTransfersMonth: Int, maxMountLimitOtherCard: Int): String {
+    return "Лимит транзакция превышен! Остаток для переводов в месяц составляет " + (maxMountLimitOtherCard - amountOfPreviousTransfersMonth) + " руб."
+}
+
+
+fun translations(amountOfPreviousTransfersMonth: Int = 0, transferAmount: Int, cardType: String = "VkPay"): String {
+    val maxNoTaxMountMaster: Int = 75_000
+    val taxMaster: Double = 0.006
+    val taxMountVisa: Double = 0.0075
+    val minCommissionVisa: Int = 35
+    val maxMountLimitVkPay = 40_000
+    val maxMountLimitOtherCard = 600_000
+    val additionalCommissionMaster = 20
+    when (cardType) {
+        "Mastercard", "Maestro" -> {
+            return if (maxMountLimitOtherCard( // Условие превышения лимита месячных транзакций
+                    amountOfPreviousTransfersMonth,
+                    transferAmount,
+                    maxMountLimitOtherCard
+                )
+            ) {
+                (maxMountLimitOtherCardText(  // текст который должен вернутся
+                    amountOfPreviousTransfersMonth,
+                    maxMountLimitOtherCard
+                ))
+
+            } else {
+                if (amountOfPreviousTransfersMonth + transferAmount < maxNoTaxMountMaster) {
+                    "Комиссия не взимается"
+                } else {
+                    val balanceAfterLimit: Int = (amountOfPreviousTransfersMonth + transferAmount) - maxNoTaxMountMaster
+                    "Комиссия " + (balanceAfterLimit * taxMaster + additionalCommissionMaster) + " руб"
+                }
+            }
+        }
+
+        "Visa", "Мир" -> {
+            return if (maxMountLimitOtherCard(amountOfPreviousTransfersMonth, transferAmount, maxMountLimitOtherCard)) {
+                (maxMountLimitOtherCardText(amountOfPreviousTransfersMonth, maxMountLimitOtherCard))
+            } else {
+                val commission: Double = transferAmount * taxMountVisa
+                if (commission < minCommissionVisa) {
+                    "Комиссия $minCommissionVisa руб"
+                } else {
+                    "Комиссия $commission руб"
+                }
+            }
+        }
+
+        else -> {
+            return if (maxMountLimitOtherCard(amountOfPreviousTransfersMonth, transferAmount, maxMountLimitVkPay)) {
+                maxMountLimitOtherCardText(amountOfPreviousTransfersMonth, maxMountLimitVkPay)
+            } else {
+                "Комиссия не взимается"
+            }
+        }
+    }
+}
+
